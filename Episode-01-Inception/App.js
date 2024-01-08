ff0a3677ba6ec3d846951a8274e3d7c7e456f2f2
Kshitij Pandey <kshitij14397@gmail.com>
2024-01-09 00:48:24 +0530

    Create nested React Elements and added CSS

--- Episode-01-Inception/App.js
+++ Episode-01-Inception/App.js
@@ -1,5 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello World");
-console.log(heading);
+const parent = React.createElement("div", {  }, [
+  React.createElement("h1", {id: "container"}, "Hello World"),
+  React.createElement("h1", {}, "Hello Kshitij"),
+]);
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
